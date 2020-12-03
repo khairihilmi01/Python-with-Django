d374ccb6a20ee9ba5e672aae1a9c46d9361ae043
khairihilmi01 <khairihilmi01@gmail.com>
2020-12-03 11:57:21 +0700

    Add database user

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from  .models import Post
 
-posts = [
-    {
-        'author':'CoreyMS',
-        'title' : 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted':'August 27, 2019'
-    },
-    {
-        'author':'Janedoe',
-        'title' : 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted':'August 28, 2019'
-    }
-
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
